38ce3ecc97849e61dd7f32dbcf17319d29bbcec1
Gabriel Haggstrom <gshaggstrom@gmail.com>
2018-07-08 23:47:06 -0300

    Implementa imageToVectors.

---
+++ imageToVectors.m
@@ -0,0 +1,39 @@
+function vectorList = imageToVectors(Image, blockSize)
+
+[numLines, numColumns] = size(Image);
+numBlockLines = ceil(numLines / blockSize);
+numBlockColumns = ceil(numColumns / blockSize);
+
+numVectors = numBlockLines * numBlockColumns;
+vectorSize = blockSize^2;
+
+vectorList = zeros(numVectors, vectorSize);
+vectorListIndex = 1;
+
+for i = 1:blockSize:numLines
+    for j = 1:blockSize:numColumns
+        vectorIndex = 1;
+        
+        for blockLine = 1:blockSize
+            for blockColumn = 1:blockSize
+                blockLineInImage   = i + blockLine   - 1;
+                blockColumnInImage = j + blockColumn - 1;
+                
+                if blockLineInImage > numLines || ...
+                        blockColumnInImage > numColumns
+                    value = 0;
+                else
+                    value = Image(blockLineInImage, blockColumnInImage);
+                end
+                
+                vectorList(vectorListIndex, vectorIndex) = value;
+                
+                vectorIndex = vectorIndex + 1;
+            end
+        end
+        
+        vectorListIndex = vectorListIndex + 1;
+    end
+end
+end
+
